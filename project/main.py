5f1b2fb54336a4e3a815488a4d9fd96f1168a6e9
EthanBehrends <epbehrends@gmail.com>
2023-10-05 15:20:34 -0500

    Initialized Python project for CartPole with REINFORCE

---
+++ project/main.py
@@ -0,0 +1,10 @@
+
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+# REINFORCE algorithm implementation will go here
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v1")
+    # Training code will go here
