1cdb1eef6626335f61c203da44189b38a9243d3c
EthanBehrends <ethan@lindy.ai>
2023-10-05 09:57:59 -0700

    commit

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+
+
+export function isEven(num) => {
+    return num % 2 === 0;
+}
+
+export function isOdd(num) {
+    if (num === 0) {
+        return false;
+    }
+    if (num === 1) {
+        return true;
+    }
+    if (num === 2) {
+        return false;
+    }
+}
