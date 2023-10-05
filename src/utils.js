249808b113f3d1aa79e57c46c658f1d37b9a1db3
EthanBehrends <epbehrends@gmail.com>
2023-10-05 17:18:42 -0500

    Fixed the isOdd function in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,8 @@
 
-
 export function isEven(num) {
     return num % 2 === 0;
 }
 
 export function isOdd(num) {
-    if (num === 0) {
-        return false;
-    }
-    if (num === 1) {
-        return true;
-    }
-    if (num === 2) {
-        return false;
-    }
+    return num % 2 !== 0;
 }
