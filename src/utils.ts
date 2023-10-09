deca5eab9f132365c503648b5436fcbfcf72e412
EthanBehrends <epbehrends@gmail.com>
2023-10-09 13:36:14 -0500

    Fixed syntax errors in utils.ts as per user request

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,12 @@
 
 export function isEven(num: number) {
-    return num: number % 2 === 0;
+    return num % 2 === 0;
 }
 
 export function isOdd(num: number) {
-    return num: number % 2 !== 0;
+    return num % 2 !== 0;
 }
 
 export function isDivisibleByThree(num: number) {
-	return num: number % 3 === 0;
+	return num % 3 === 0;
 }
