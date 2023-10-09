4754936d25d608722572366220836fb518929181
EthanBehrends <epbehrends@gmail.com>
2023-10-09 13:39:31 -0500

    Added utils.test.ts file to test the fibonacci function as per user request

---
+++ src/utils.test.ts
@@ -0,0 +1,10 @@
+
+import { fibonacci } from './utils';
+
+test('fibonacci function', () => {
+    expect(fibonacci(0)).toBe(0);
+    expect(fibonacci(1)).toBe(1);
+    expect(fibonacci(2)).toBe(1);
+    expect(fibonacci(3)).toBe(2);
+    expect(fibonacci(10)).toBe(55);
+});
